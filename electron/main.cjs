77e640e472913b16a4f9b64a7283992c78857443
Heaifan <3501337858@qq.com>
2026-05-05 14:08:00 +0800

    Refactor project structure by removing unused tactical-rts.html file and its related logic.

---
+++ electron/main.cjs
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.ELECTRON_DEV === '1';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1180,
+    height: 820,
+    minWidth: 900,
+    minHeight: 700,
+    title: '硬核战术规划模拟器',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    show: false,
+  });
+
+  win.once('ready-to-show', () => win.show());
+
+  if (isDev) {
+    win.loadURL('http://127.0.0.1:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
